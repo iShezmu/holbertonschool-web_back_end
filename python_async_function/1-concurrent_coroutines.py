3a0c04febaa97f400b1efa3e75510516ccc13c9f
iShezmu <arteaga25610@gmail.com>
2024-01-28 18:39:58 -0800

    update: new lines added.

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 """write an async routine called wait_n"""
 
-import typing
+import asyncio
+import random
+from typing import List
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> typing.List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Function that returns list of all the delays"""
-    l: typing.List[float] = []
+    delays = []
     for _ in range(n):
         task = await wait_random(max_delay)
-        l.append(task)
-    return sorted(l)
+        delays.append(task)
+    return sorted(delays)
